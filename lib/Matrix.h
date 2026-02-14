8b35c33cbceeb583ef4b3bedd7241f099ea85b05
Piotr Ackermann <pioacker@gmail.com>
2026-02-14 18:00:23 +0100

    Matrix multiply

--- lib/Matrix.h
+++ lib/Matrix.h
@@ -3,15 +3,15 @@
 #include <cstddef>
 #include <ostream>
 #include <stdexcept>
+#include <initializer_list>
+
+#include "Tuple.h"
 
 namespace rt {
     template<std::size_t N>
     class Matrix {
     public:
         constexpr Matrix() : data_{} {
-            for (std::size_t i = 0; i < N; ++i) {
-                data_[N * i + i] = 1.0;
-            }
         }
 
         constexpr Matrix(std::initializer_list<double> data) {
@@ -37,6 +37,20 @@ namespace rt {
             return !(a == b);
         }
 
+        friend constexpr Matrix operator*(const Matrix &a, const Matrix &b) {
+            Matrix m;
+            for (std::size_t i = 0; i < N; ++i) {
+                for (std::size_t j = 0; j < N; ++j) {
+                    double sum = 0;
+                    for (std::size_t k = 0; k < N; ++k) {
+                        sum += a(i, k) * b(k, j);
+                    }
+                    m(i, j) = sum;
+                }
+            }
+            return m;
+        }
+
         friend std::ostream &operator<<(std::ostream &os, const Matrix &obj) {
             os << "|";
             for (std::size_t i = 0; i < N; ++i) {
@@ -51,4 +65,13 @@ namespace rt {
     private:
         std::array<double, N * N> data_;
     };
+
+    constexpr Tuple operator*(const Matrix<4> &a, const Tuple &t) {
+        return {
+            a(0, 0) * t.x + a(0, 1) * t.y + a(0, 2) * t.z + a(0, 3) * t.w,
+            a(1, 0) * t.x + a(1, 1) * t.y + a(1, 2) * t.z + a(1, 3) * t.w,
+            a(2, 0) * t.x + a(2, 1) * t.y + a(2, 2) * t.z + a(2, 3) * t.w,
+            a(3, 0) * t.x + a(3, 1) * t.y + a(3, 2) * t.z + a(3, 3) * t.w,
+        };
+    }
 }

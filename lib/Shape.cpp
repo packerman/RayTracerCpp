049be6b7ac98b662dbfd53eb90efb22f5b44f1cc
Piotr Ackermann <pioacker@gmail.com>
2026-03-15 00:36:58 +0100

    Cylinders (#9)

--- lib/Shape.cpp
+++ lib/Shape.cpp
@@ -4,13 +4,15 @@
 #include <cmath>
 #include <algorithm>
 
+using namespace std;
+
 namespace rt {
-    std::vector<Intersection> Shape::intersect(const Ray &ray) {
+    std::vector<Intersection> Shape::intersect(const Ray& ray) {
         const auto local_ray = ray.transform(inversed_transform());
         return local_intersect(local_ray);
     }
 
-    Vector Shape::normal_at(const Point &point) const {
+    Vector Shape::normal_at(const Point& point) const {
         const auto local_point = inversed_transform() * point;
         const auto local_normal = local_normal_at(local_point);
         auto world_normal = inversed_transform().transpose() * local_normal;
@@ -18,7 +20,7 @@ namespace rt {
         return world_normal.normalize();
     }
 
-    std::vector<Intersection> Sphere::local_intersect(const Ray &ray) {
+    std::vector<Intersection> Sphere::local_intersect(const Ray& ray) {
         const Vector sphere_to_ray = ray.origin() - point(0, 0, 0);
 
         const auto a = ray.direction().dot(ray.direction());
@@ -31,42 +33,42 @@ namespace rt {
             return {};
         }
 
-        const auto t1 = (-b - std::sqrt(discriminant)) / (2 * a);
-        const auto t2 = (-b + std::sqrt(discriminant)) / (2 * a);
+        const auto t1 = (-b - sqrt(discriminant)) / (2 * a);
+        const auto t2 = (-b + sqrt(discriminant)) / (2 * a);
         return {{t1, this}, {t2, this}};
     }
 
-    Vector Sphere::local_normal_at(const Point &local_point) const {
+    Vector Sphere::local_normal_at(const Point& local_point) const {
         return local_point - point(0, 0, 0);
     }
 
-    std::unique_ptr<Sphere> sphere() {
-        return std::make_unique<Sphere>();
+    std::unique_ptr<Shape> sphere() {
+        return make_unique<Sphere>();
     }
 
-    std::vector<Intersection> Plane::local_intersect(const Ray &ray) {
-        if (std::abs(ray.direction().y) < std::numeric_limits<double>::epsilon()) {
+    std::vector<Intersection> Plane::local_intersect(const Ray& ray) {
+        if (std::abs(ray.direction().y) < numeric_limits<double>::epsilon()) {
             return {};
         }
         auto t = -ray.origin().y / ray.direction().y;
         return {{t, this}};
     }
 
-    Vector Plane::local_normal_at(const Point &point) const {
+    Vector Plane::local_normal_at(const Point& point) const {
         return vector(0, 1, 0);
     }
 
-    std::unique_ptr<Plane> plane() {
-        return std::make_unique<Plane>();
+    std::unique_ptr<Shape> plane() {
+        return make_unique<Plane>();
     }
 
-    std::vector<Intersection> Cube::local_intersect(const Ray &ray) {
+    std::vector<Intersection> Cube::local_intersect(const Ray& ray) {
         auto [xt_min, xt_max] = check_axis(ray.origin().x, ray.direction().x);
         auto [yt_min, yt_max] = check_axis(ray.origin().y, ray.direction().y);
         auto [zt_min, zt_max] = check_axis(ray.origin().z, ray.direction().z);
 
-        auto t_min = std::max({xt_min, yt_min, zt_min});
-        auto t_max = std::min({xt_max, yt_max, zt_max});
+        auto t_min = max({xt_min, yt_min, zt_min});
+        auto t_max = min({xt_max, yt_max, zt_max});
 
         if (t_min > t_max) {
             return {};
@@ -75,13 +77,13 @@ namespace rt {
         return {{t_min, this}, {t_max, this}};
     }
 
-    Vector Cube::local_normal_at(const Point &local_point) const {
-        const auto max_c = std::max({std::abs(local_point.x), std::abs(local_point.y), std::abs(local_point.z)});
+    Vector Cube::local_normal_at(const Point& local_point) const {
+        const auto max_c = max({abs(local_point.x), abs(local_point.y), abs(local_point.z)});
 
-        if (max_c == std::abs(local_point.x)) {
+        if (max_c == abs(local_point.x)) {
             return vector(local_point.x, 0, 0);
         }
-        if (max_c == std::abs(local_point.y)) {
+        if (max_c == abs(local_point.y)) {
             return vector(0, local_point.y, 0);
         }
         return vector(0, 0, local_point.z);
@@ -95,13 +97,136 @@ namespace rt {
         auto t_max = t_max_numerator / direction;
 
         if (t_min > t_max) {
-            std::swap(t_min, t_max);
+            swap(t_min, t_max);
         }
 
         return {t_min, t_max};
     }
 
-    std::unique_ptr<Cube> cube() {
-        return std::make_unique<Cube>();
+    std::unique_ptr<Shape> cube() {
+        return make_unique<Cube>();
+    }
+
+    std::vector<Intersection> Cylinder::local_intersect(const Ray& ray) {
+        std::vector<Intersection> xs;
+        if (const auto a = pow(ray.direction().x, 2) + pow(ray.direction().z, 2);
+            a > numeric_limits<double>::epsilon()) {
+            const auto b = 2 * ray.origin().x * ray.direction().x + 2 * ray.origin().z * ray.direction().z;
+            const auto c = pow(ray.origin().x, 2) + pow(ray.origin().z, 2) - 1;
+            if (const auto disc = b * b - 4 * a * c; disc >= 0) {
+                auto t0 = (-b - sqrt(disc)) / (2 * a);
+                auto t1 = (-b + sqrt(disc)) / (2 * a);
+
+                if (const auto y0 = ray.origin().y + t0 * ray.direction().y; minimum_ < y0 && y0 < maximum_) {
+                    xs.emplace_back(t0, this);
+                }
+
+                if (const auto y1 = ray.origin().y + t1 * ray.direction().y; minimum_ < y1 && y1 < maximum_) {
+                    xs.emplace_back(t1, this);
+                }
+            }
+        }
+
+        intersect_caps(ray, xs);
+
+        return xs;
+    }
+
+    Vector Cylinder::local_normal_at(const Point& local_point) const {
+        const auto dist = pow(local_point.x, 2) + pow(local_point.z, 2);
+
+        if (dist < 1 && local_point.y >= maximum_ - numeric_limits<double>::epsilon()) {
+            return vector(0, 1, 0);
+        }
+        if (dist < 1 && local_point.y <= minimum_ + numeric_limits<double>::epsilon()) {
+            return vector(0, -1, 0);
+        }
+        return vector(local_point.x, 0, local_point.z);
+    }
+
+    void Cylinder::intersect_caps(const Ray& ray, std::vector<Intersection>& xs) {
+        if (!closed_ || abs(ray.direction().y) <= numeric_limits<double>::epsilon()) {
+            return;
+        }
+        auto t = (minimum_ - ray.origin().y) / ray.direction().y;
+        if (check_cap(ray, t)) {
+            xs.emplace_back(t, this);
+        }
+
+        t = (maximum_ - ray.origin().y) / ray.direction().y;
+        if (check_cap(ray, t)) {
+            xs.emplace_back(t, this);
+        }
+    }
+
+    bool Cylinder::check_cap(const Ray& ray, const double t) {
+        const auto x = ray.origin().x + t * ray.direction().x;
+        const auto z = ray.origin().z + t * ray.direction().z;
+
+        return x * x + z * z <= 1;
+    }
+
+    std::unique_ptr<Shape> cylinder(double minimum, double maximum, bool closed) {
+        return make_unique<Cylinder>(minimum, maximum, closed);
+    }
+
+    std::vector<Intersection> Cone::local_intersect(const Ray& ray) {
+        std::vector<Intersection> xs;
+
+        const auto a = pow(ray.direction().x, 2) - pow(ray.direction().y, 2) + pow(ray.direction().z, 2);
+        const auto b = 2 * ray.origin().x * ray.direction().x - 2 * ray.origin().y * ray.direction().y
+                       + 2 * ray.origin().z * ray.direction().z;
+        const auto c = pow(ray.origin().x, 2) - pow(ray.origin().y, 2) + pow(ray.origin().z, 2);
+        if (abs(a) > numeric_limits<double>::epsilon()) {
+            if (const auto disc = b * b - 4 * a * c; disc >= 0) {
+                auto t0 = (-b - sqrt(disc)) / (2 * a);
+                auto t1 = (-b + sqrt(disc)) / (2 * a);
+
+                if (const auto y0 = ray.origin().y + t0 * ray.direction().y; minimum_ < y0 && y0 < maximum_) {
+                    xs.emplace_back(t0, this);
+                }
+
+                if (const auto y1 = ray.origin().y + t1 * ray.direction().y; minimum_ < y1 && y1 < maximum_) {
+                    xs.emplace_back(t1, this);
+                }
+            }
+        } else {
+            auto t = -c / (2 * b);
+            xs.emplace_back(t, this);
+        }
+
+        intersect_caps(ray, xs);
+
+        return xs;
+    }
+
+    Vector Cone::local_normal_at(const Point& local_point) const {
+        return {};
+    }
+
+    void Cone::intersect_caps(const Ray& ray, std::vector<Intersection>& xs) {
+        if (!closed_ || abs(ray.direction().y) <= numeric_limits<double>::epsilon()) {
+            return;
+        }
+        auto t = (minimum_ - ray.origin().y) / ray.direction().y;
+        if (check_cap(ray, t, minimum_)) {
+            xs.emplace_back(t, this);
+        }
+
+        t = (maximum_ - ray.origin().y) / ray.direction().y;
+        if (check_cap(ray, t, maximum_)) {
+            xs.emplace_back(t, this);
+        }
+    }
+
+    bool Cone::check_cap(const Ray& ray, const double t, const double y) {
+        const auto x = ray.origin().x + t * ray.direction().x;
+        const auto z = ray.origin().z + t * ray.direction().z;
+
+        return x * x + z * z <= y * y;
+    }
+
+    std::unique_ptr<Shape> cone(double minimum, double maximum, bool closed) {
+        return make_unique<Cone>(minimum, maximum, closed);
     }
 }
--- lib/Shape.h
+++ lib/Shape.h
@@ -15,23 +15,23 @@ namespace rt {
 
         virtual ~Shape() = default;
 
-        Shape(const Shape &other) = delete;
+        Shape(const Shape& other) = delete;
 
-        Shape(Shape &&other) noexcept = delete;
+        Shape(Shape&& other) noexcept = delete;
 
-        Shape &operator=(const Shape &other) = delete;
+        Shape& operator=(const Shape& other) = delete;
 
-        Shape &operator=(Shape &&other) noexcept = delete;
+        Shape& operator=(Shape&& other) noexcept = delete;
 
-        [[nodiscard]] const Transformation &transform() const {
+        [[nodiscard]] const Transformation& transform() const {
             return transform_;
         }
 
-        [[nodiscard]] const Transformation &inversed_transform() const {
+        [[nodiscard]] const Transformation& inversed_transform() const {
             return inversed_transform_;
         }
 
-        void set_transform(const Transformation &transform) {
+        void set_transform(const Transformation& transform) {
             transform_ = transform;
             if (const auto inversed = transform.inverse(); inversed) {
                 inversed_transform_ = inversed.value();
@@ -40,25 +40,25 @@ namespace rt {
             }
         }
 
-        [[nodiscard]] const Material &material() const {
+        [[nodiscard]] const Material& material() const {
             return material_;
         }
 
-        [[nodiscard]] Material &material() {
+        [[nodiscard]] Material& material() {
             return material_;
         }
 
-        void set_material(const Material &material) {
+        void set_material(const Material& material) {
             material_ = material;
         }
 
-        std::vector<Intersection> intersect(const Ray &ray);
+        std::vector<Intersection> intersect(const Ray& ray);
 
-        virtual std::vector<Intersection> local_intersect(const Ray &ray) = 0;
+        virtual std::vector<Intersection> local_intersect(const Ray& ray) = 0;
 
-        [[nodiscard]] Vector normal_at(const Point &point) const;
+        [[nodiscard]] Vector normal_at(const Point& point) const;
 
-        [[nodiscard]] virtual Vector local_normal_at(const Point &local_point) const = 0;
+        [[nodiscard]] virtual Vector local_normal_at(const Point& local_point) const = 0;
 
     private:
         Transformation transform_{Transformation::identity()};
@@ -70,39 +70,103 @@ namespace rt {
     public:
         Sphere() = default;
 
-        Sphere(const Sphere &other) = delete;
+        Sphere(const Sphere& other) = delete;
 
-        Sphere(Sphere &&other) noexcept = delete;
+        Sphere(Sphere&& other) noexcept = delete;
 
-        Sphere &operator=(const Sphere &other) = delete;
+        Sphere& operator=(const Sphere& other) = delete;
 
-        Sphere &operator=(Sphere &&other) noexcept = delete;
+        Sphere& operator=(Sphere&& other) noexcept = delete;
 
-        std::vector<Intersection> local_intersect(const Ray &ray) override;
+        std::vector<Intersection> local_intersect(const Ray& ray) override;
 
-        [[nodiscard]] Vector local_normal_at(const Point &local_point) const override;
+        [[nodiscard]] Vector local_normal_at(const Point& local_point) const override;
     };
 
-    std::unique_ptr<Sphere> sphere();
+    std::unique_ptr<Shape> sphere();
 
     class Plane : public Shape {
     public:
-        std::vector<Intersection> local_intersect(const Ray &ray) override;
+        std::vector<Intersection> local_intersect(const Ray& ray) override;
 
-        [[nodiscard]] Vector local_normal_at(const Point &point) const override;
+        [[nodiscard]] Vector local_normal_at(const Point& point) const override;
     };
 
-    std::unique_ptr<Plane> plane();
+    std::unique_ptr<Shape> plane();
 
     class Cube : public Shape {
     public:
-        std::vector<Intersection> local_intersect(const Ray &ray) override;
+        std::vector<Intersection> local_intersect(const Ray& ray) override;
 
-        [[nodiscard]] Vector local_normal_at(const Point &local_point) const override;
+        [[nodiscard]] Vector local_normal_at(const Point& local_point) const override;
 
     private:
         static std::pair<double, double> check_axis(double origin, double direction);
     };
 
-    std::unique_ptr<Cube> cube();
+    std::unique_ptr<Shape> cube();
+
+    class Cylinder : public Shape {
+    public:
+        Cylinder(const double minimum, const double maximum, const bool closed)
+            : minimum_(minimum),
+              maximum_(maximum),
+              closed_(closed) {
+        }
+
+        std::vector<Intersection> local_intersect(const Ray& ray) override;
+
+        [[nodiscard]] Vector local_normal_at(const Point& local_point) const override;
+
+        [[nodiscard]] double minimum() const {
+            return minimum_;
+        }
+
+        [[nodiscard]] double maximum() const {
+            return maximum_;
+        }
+
+        [[nodiscard]] bool closed() const {
+            return closed_;
+        }
+
+    private:
+        double minimum_;
+        double maximum_;
+        bool closed_;
+
+        void intersect_caps(const Ray& ray, std::vector<Intersection>& xs);
+
+        static bool check_cap(const Ray& ray, double t);
+    };
+
+    std::unique_ptr<Shape> cylinder(double minimum = -std::numeric_limits<double>::infinity(),
+                                    double maximum = std::numeric_limits<double>::infinity(),
+                                    bool closed = false);
+
+    class Cone : public Shape {
+    public:
+        Cone(const double minimum, const double maximum, const bool closed)
+            : minimum_(minimum),
+              maximum_(maximum),
+              closed_(closed) {
+        }
+
+        std::vector<Intersection> local_intersect(const Ray& ray) override;
+
+        [[nodiscard]] Vector local_normal_at(const Point& local_point) const override;
+
+    private:
+        double minimum_;
+        double maximum_;
+        bool closed_;
+
+        void intersect_caps(const Ray& ray, std::vector<Intersection>& xs);
+
+        static bool check_cap(const Ray& ray, double t, double y);
+    };
+
+    std::unique_ptr<Shape> cone(double minimum = -std::numeric_limits<double>::infinity(),
+                                double maximum = std::numeric_limits<double>::infinity(),
+                                bool closed = false);
 }
